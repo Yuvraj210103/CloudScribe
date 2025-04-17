721143c7840195fc2b436688bd4a22e264515fda
Yuvraj210103 <ys146228@gmail.com>
2025-04-17 14:57:46 +0530

    feat: created signout feature

---
+++ src/components/common/ConfirmDialog.tsx
@@ -0,0 +1,33 @@
+import DialogDrawer from './DialogDrawer';
+
+interface ConfirmDialogProps {
+  open: boolean;
+  setOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  title?: string;
+  children: React.ReactNode;
+  positiveCallback: () => void;
+  negativeCallback?: () => void;
+}
+
+const ConfirmDialog = ({
+  negativeCallback,
+  positiveCallback,
+  open,
+  setOpened,
+  title = 'Confirm',
+  children,
+}: ConfirmDialogProps) => {
+  return (
+    <DialogDrawer
+      opened={open}
+      setOpened={setOpened}
+      title={title}
+      positiveCallback={positiveCallback}
+      isDialogFooterReq
+      negativeCallback={negativeCallback}>
+      {children}
+    </DialogDrawer>
+  );
+};
+
+export default ConfirmDialog;
--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import ThemeToggle from '../ThemeToggle';
+import { LogOutIcon } from 'lucide-react';
+import ConfirmDialog from '../common/ConfirmDialog';
+import { signOut } from 'next-auth/react';
 
 const Nav = () => {
+  const [opened, setOpened] = useState(false);
   return (
     <div className="flex items-center justify-center w-full lg:px-36 px-4 py-2 bg-[rgba(0,0,0,0.2)]">
       <div className="flex justify-between w-full max-w-[1280px]">
         <div className="font-semibold text-xl">CloudScribe</div>
-        <ThemeToggle />
+
+        <div className="flex items-center gap-6">
+          <ThemeToggle />
+
+          <LogOutIcon className="cursor-pointer" onClick={() => setOpened(!opened)} />
+
+          <ConfirmDialog
+            open={opened}
+            setOpened={setOpened}
+            positiveCallback={() => {
+              signOut();
+            }}>
+            <p>Are you sure you want to logout?</p>
+          </ConfirmDialog>
+        </div>
       </div>
     </div>
   );
