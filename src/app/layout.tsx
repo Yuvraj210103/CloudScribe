7135698e8caaeab7ea3cd8f96e82094f77b6b34d
Yuvraj210103 <ys146228@gmail.com>
2025-04-18 11:27:17 +0530

    fix: auth error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Layout from '@/components/layout';
 import { Toaster } from 'sonner';
 import ClientProvider from '@/provider/client-provider';
 import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/utils';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
